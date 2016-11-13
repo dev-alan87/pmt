a49977710b98de9f73d7cc919ecf509030898c49
vvteixeira <victor_vequetine@hotmail.com>
2016-11-13 20:39:28 -0200

    implementacao das interfaces do teste CDR

---
+++ PocketMentalTest/app/src/main/java/br/edu/ufabc/padm/pocketmentaltest/CDRAddActivity.java
@@ -0,0 +1,22 @@
+package br.edu.ufabc.padm.pocketmentaltest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class CDRAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cdr_add);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO ajustar o menu?
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
